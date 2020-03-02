5394abe8eeb3221b4b83f5070d2db3af4b841334
Alex Allman <xalexxd@hotmail.co.uk>
2020-03-02 10:52:16 +0000

    Point light reworked, start for the mirror needed to done

--- SFML-Raytracer/source/PointLight.cpp
+++ SFML-Raytracer/source/PointLight.cpp
@@ -22,41 +22,37 @@ void PointLight::CalculateLighting(const AA::Ray& inRay, Hittable::HitResult& re
     //    ++_currentRecursion;
     //}
 
-    //Just does a check purely based on collisions to create a shadow ray
-    Hittable::HitResult staticRes, dynamicRes, lightRes;
+    Hittable::HitResult staticRes, dynamicRes;
     bool staticHit = false;
     bool dynamicHit = false;
 
-    //Build a ray that fires from the point of collision to the light
+    //Create the collision point and material calc as they will be used more than once, set up the other vars for later use
     AA::Vec3 collisionPoint = res.p;
     AA::Ray outRay = AA::Ray(collisionPoint, AA::Vec3::UnitVector(_position - collisionPoint));
+
+    //Adjust outray to match the new position its sampled to and shift it slightly along its normal
     outRay._startPos = outRay.GetPointAlongRay(AA::kEpsilon);
+
+    //Do the material calc based on the new data from the new outRay
+    AA::Vec3 materialCalc = res.mat->MaterialActive() ? res.mat->MaterialCalculatedColour(inRay, res, this) : AA::Vec3(res.col.r / 255, res.col.g / 255, res.col.b / 255);
 
     //Check if the dot of the hit max zero returns zero and if it does the light calc doesnt need to be done as the normal is the opposide side to the light ray
     double nDotDHit = std::max(res.normal.DotProduct(outRay._dir), 0.0);
-    if (nDotDHit == 0.0)
-    {
-        res.col = _shadowColour;
-        return;
-    }
 
-    //Find the t of this ray
-    IntersectedRayOnly(outRay, 0, INFINITY, lightRes);
+    //Otherwise move onto the visibility check
+    //Calc the distance from hit to light
+    double dist = collisionPoint.Distance(_position);
 
     //Check against a hit with both static and dynamics
-    staticHit = _statics == nullptr ? false : _statics->IntersectedRayOnly(outRay, 0.0, lightRes.t, staticRes);
-    dynamicHit = _dynamics == nullptr ? false : _dynamics->IntersectedRayOnly(outRay, 0.0, lightRes.t, dynamicRes);
+    staticHit = _statics == nullptr ? false : _statics->IntersectedRayOnly(outRay, 0.0, dist, staticRes);
+    dynamicHit = _dynamics == nullptr ? false : _dynamics->IntersectedRayOnly(outRay, 0.0, dist, dynamicRes);
 
-    if (!staticHit && !dynamicHit || isRecursive)
+    if (!staticHit && !dynamicHit)
     {
-        //Material colour properties, just default to the colour calc'd from the ray if its not using the material, most of the time its a colour based off the normal
-        AA::Vec3 materialCalc = res.mat->MaterialActive() ? res.mat->MaterialCalculatedColour(inRay, res, this) : AA::Vec3(res.col.r / 255, res.col.g / 255, res.col.b / 255);
-
-        //Calc final colour
-        AA::Vec3 finalColourVec = (nDotDHit /  (lightRes.t * lightRes.t)) * materialCalc * _lightColorVec * _intensityMod;
+        AA::Vec3 reflectance = (nDotDHit / (dist * dist)) * materialCalc * _lightColorVec * _intensityMod;
 
         //Tonemap using the selected method and set the colour
-        res.col = AA::GammaTonemap(finalColourVec);
+        res.col = AA::GammaTonemap(reflectance);
         return;
     }
 
@@ -65,42 +61,38 @@ void PointLight::CalculateLighting(const AA::Ray& inRay, Hittable::HitResult& re
 
 AA::Vec3 PointLight::CalculateLightingForMaterial(const AA::Ray& inRay, Hittable::HitResult& res)
 {
-    Hittable::HitResult staticRes, dynamicRes, lightRes;
+    Hittable::HitResult staticRes, dynamicRes;
     bool staticHit = false;
     bool dynamicHit = false;
 
-    //Build a ray that fires from the point of collision to the light
+    //Create the collision point and material calc as they will be used more than once, set up the other vars for later use
     AA::Vec3 collisionPoint = res.p;
     AA::Ray outRay = AA::Ray(collisionPoint, AA::Vec3::UnitVector(_position - collisionPoint));
+
+    //Adjust outray to match the new position its sampled to and shift it slightly along its normal
     outRay._startPos = outRay.GetPointAlongRay(AA::kEpsilon);
+
+    //Do the material calc based on the new data from the new outRay
+    AA::Vec3 materialCalc = res.mat->MaterialActive() ? res.mat->MaterialCalculatedColour(inRay, res, this) : AA::Vec3(res.col.r / 255, res.col.g / 255, res.col.b / 255);
 
     //Check if the dot of the hit max zero returns zero and if it does the light calc doesnt need to be done as the normal is the opposide side to the light ray
     double nDotDHit = std::max(res.normal.DotProduct(outRay._dir), 0.0);
-    if (nDotDHit == 0.0)
-    {
-        res.col = _shadowColour;
-        return;
-    }
 
-    //Find the t of this ray
-    IntersectedRayOnly(outRay, 0, INFINITY, lightRes);
+    //Otherwise move onto the visibility check
+    //Calc the distance from hit to light
+    double dist = collisionPoint.Distance(_position);
 
     //Check against a hit with both static and dynamics
-    staticHit = _statics == nullptr ? false : _statics->IntersectedRayOnly(outRay, 0.0, lightRes.t, staticRes);
-    dynamicHit = _dynamics == nullptr ? false : _dynamics->IntersectedRayOnly(outRay, 0.0, lightRes.t, dynamicRes);
+    staticHit = _statics == nullptr ? false : _statics->IntersectedRayOnly(outRay, 0.0, dist, staticRes);
+    dynamicHit = _dynamics == nullptr ? false : _dynamics->IntersectedRayOnly(outRay, 0.0, dist, dynamicRes);
 
     if (!staticHit && !dynamicHit)
     {
-        //Material colour properties, just default to the colour calc'd from the ray if its not using the material, most of the time its a colour based off the normal
-        AA::Vec3 materialCalc = res.mat->MaterialActive() ? res.mat->MaterialCalculatedColour(inRay, res, this) : AA::Vec3(res.col.r / 255, res.col.g / 255, res.col.b / 255);
-
-        //Calc final colour
-        AA::Vec3 finalColourVec = (nDotDHit / (lightRes.t * lightRes.t)) * materialCalc * _lightColorVec * _intensityMod;
+        AA::Vec3 reflectance = (nDotDHit / (dist * dist)) * materialCalc * _lightColorVec * _intensityMod;
 
         //Tonemap using the selected method and set the colour
-        res.col = AA::GammaTonemap(finalColourVec);
-        return;
+        return reflectance;
     }
 
-    res.col = _shadowColour;
+    return AA::colToVec3(_shadowColour);
 }
